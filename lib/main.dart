bd5e9e51e4ecbaed6ba9a59df96ec86711fdfca8
pc <harshilmoradiya1244@gmail.com>
2023-11-22 23:55:31 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ios_contact/Screen/contact_info/view/contact_info_screen.dart';
 import 'package:ios_contact/Screen/contact_info/view/contact_info_screen_android.dart';
-import 'package:ios_contact/Screen/home/view/home_screen.dart';
 import 'package:ios_contact/Screen/home/view/home_screen_android.dart';
+import 'Screen/contact_info/view/contact_info_screen.dart';
+import 'Screen/home/view/home_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+   Platform.isAndroid ? MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
         '/':(context) => HomeScreenAndroid(),
         'info':(context) => ContactInfoScreenAndroid(),
       },
-    )
-    // CupertinoApp(
-    //   debugShowCheckedModeBanner: false,
-    //   routes: {
-    //     // '/':(p0) =>HomeScreen(),
-    //     // 'info':(p0) => ContactInfoscreen(),
-    //     '
-    //   },
-    // )
+    ):
+    CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(p0) =>HomeScreen(),
+        'info':(p0) => ContactInfoscreen(),
+      },
+     )
   );
 }
